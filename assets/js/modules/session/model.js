76128ac58757604f4103798ae0bfcdb8e5a62a12
Dendrox <rodgy_kirwan@yahoo.co.uk>
2015-10-27 19:39:52 +0000

    changed

--- assets/js/modules/session/model.js
+++ assets/js/modules/session/model.js
@@ -12,7 +12,7 @@ Model = Backbone.Model.extend({
 		return response;
 	},
 	urlRoot : function(){
-		return 'https://git.heroku.com/intense-thicket-2598.git/api/authenticate'
+		return 'https://intense-thicket-2598.herokuapp.com/api/authenticate'
 		//add token to URL: ?token=' + window.App.data.token
 	}
 });
